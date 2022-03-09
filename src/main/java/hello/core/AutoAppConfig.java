18c03bc7904801bfb2a9dfa605907cf04dcca207
junepro <57428866+junepro@users.noreply.github.com>
2022-03-09 20:44:49 +0900

    component scan

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,22 @@
+package hello.core;
+
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        basePackages = "hello.core.member", //찾는 스타트위치 지정
+        excludeFilters = @ComponentScan.Filter(
+                type = FilterType.ANNOTATION, classes = Configuration.class)
+)//configuration 클래슨 제외
+public class AutoAppConfig {
+
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
